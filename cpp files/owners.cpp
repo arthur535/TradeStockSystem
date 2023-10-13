4f8432ebebb48f470bc1681a0113b004db635b22
Heisenberg <109768662+arthur535@users.noreply.github.com>
2023-10-14 02:29:51 +0400

    Add files via upload

---
+++ cpp files/owners.cpp
@@ -0,0 +1,18 @@
+#include "/Users/arthur/Desktop/trade/owners.h"
+#include "/Users/arthur/Desktop/trade/stock.hpp"
+#include "/Users/arthur/Desktop/trade/trader.hpp"
+
+
+
+void Owners::add_record(Trader& trader,Stock stock, int quantity) {
+    owners[trader.get_id()][stock] = quantity;
+    trader.add_to_portfolio(stock, quantity);
+}
+int Owners::get_stock_quantity(Trader& trader,Stock& stock) {
+    if(trader.get_id() >= owners.size())
+        throw std::invalid_argument("trader argument is not valid");
+    return owners[trader.get_id()][stock];
+}
+std::size_t Owners::StockHash::operator()(const Stock& stock) const {
+    return std::hash<std::string>()(stock.get_symbol());
+}
